f4bcb1801aacd60e0f01c4295aa6e2a5f575b12e
Matthieu Hocquart <matthieu@hocquart.fr>
2024-05-09 18:44:48 +0200

    Deploy Marketplace

---
+++ script/deployment/2_Marketplace.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
+import {Marketplace} from "@/Marketplace_1.0.0.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+contract DeployMarketplace is Script {
+    function getDeployedNFTAddress() internal view returns (address) {
+        string memory path = string.concat(
+            vm.projectRoot(), "/broadcast/1_NFT.s.sol/", Strings.toString(vm.envUint("CHAIN_ID")), "/run-latest.json"
+        );
+        return stdJson.readAddress(vm.readFile(path), ".transactions[0].contractAddress");
+    }
+
+    function run() external {
+        address tokenAddress = vm.envAddress("TOKEN_ADDRESS");
+        address nftAddress = getDeployedNFTAddress();
+        address admin = vm.envAddress("ADMIN_TIMELOCK_2D");
+        address feeManager = admin;
+        address feeReceiver = vm.envAddress("MARKETPLACE_FEE_RECEIVER");
+        uint8 sellPercentFee = uint8(vm.envUint("MARKETPLACE_SELL_PERCENT_FEE"));
+        uint8 buyPercentFee = uint8(vm.envUint("MARKETPLACE_BUY_PERCENT_FEE"));
+        uint8 burnPercentFee = uint8(vm.envUint("MARKETPLACE_BURN_PERCENT_FEE"));
+
+        vm.startBroadcast();
+
+        require(msg.sender != admin, "Deployer cannot be the admin wallet");
+        require(msg.sender != feeManager, "Deployer cannot be the feeManager wallet");
+
+        address proxy = Upgrades.deployTransparentProxy(
+            "Marketplace_1.0.0.sol:Marketplace",
+            admin,
+            abi.encodeCall(Marketplace.initialize, (tokenAddress, nftAddress))
+        );
+        Marketplace marketplace = Marketplace(proxy);
+        require(
+            marketplace.hasRole(marketplace.DEFAULT_ADMIN_ROLE(), msg.sender), "Expect DEFAULT_ADMIN_ROLE on Deployer"
+        );
+        marketplace.setMarketplaceFeesReceiver(feeReceiver);
+        marketplace.setMarketplacePercentFees(sellPercentFee, buyPercentFee, burnPercentFee);
+        marketplace.grantRole(marketplace.DEFAULT_ADMIN_ROLE(), admin);
+        marketplace.grantRole(marketplace.FEE_MANAGER_ROLE(), feeManager);
+        marketplace.renounceRole(marketplace.FEE_MANAGER_ROLE(), msg.sender);
+        marketplace.grantRole(marketplace.DEFAULT_ADMIN_ROLE(), msg.sender);
+
+        vm.stopBroadcast();
+    }
+}
